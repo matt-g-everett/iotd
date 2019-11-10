7c0c739d1553aca78e321668ea25482a9af6f05d
Matt Everett <matt.g.everett@gmail.com>
2019-11-10 18:06:49 +0000

    Send large example data to test CRC32.

--- main.go
+++ main.go
@@ -2,26 +2,39 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     "log"
     "os"
     "time"
+    "reflect"
 
     "github.com/eclipse/paho.mqtt.golang"
 )
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-    fmt.Printf("TOPIC: %s\n", msg.Topic())
-    fmt.Printf("MSG: %s\n", msg.Payload())
+    fmt.Printf("SUBSCRIBE TOPIC: %s\n", msg.Topic())
+    fmt.Printf("SUBSCRIBE MSG: %s\n", msg.Payload())
+    fmt.Printf("SUBSCRIBE MSGID: %d\n", msg.MessageID())
 }
 
 func main() {
     mqtt.DEBUG = log.New(os.Stdout, "", 0)
     mqtt.ERROR = log.New(os.Stdout, "", 0)
 
-    opts := mqtt.NewClientOptions().AddBroker("tcp://***REMOVED***:31883").SetClientID("iotd").SetUsername("***REMOVED***").SetPassword("***REMOVED***")
-    opts.SetKeepAlive(2 * time.Second)
-    opts.SetDefaultPublishHandler(f)
-    opts.SetPingTimeout(1 * time.Second)
+    opts := mqtt.NewClientOptions().
+        AddBroker("tcp://***REMOVED***:31883").
+        SetClientID("iotd").
+        SetUsername("***REMOVED***").
+        SetPassword("***REMOVED***").
+        SetKeepAlive(30 * time.Second).
+        SetDefaultPublishHandler(f).
+        SetPingTimeout(5 * time.Second)
 
     c := mqtt.NewClient(opts)
     if token := c.Connect(); token.Wait() && token.Error() != nil {
@@ -33,9 +46,15 @@ func main() {
         os.Exit(1)
     }
 
+    dat, err := ioutil.ReadFile("data/lorem.txt")
+    check(err)
+    fmt.Print(string(dat))
+
     for {
-        fmt.Println("hello world")
-        token := c.Publish("home/upgrade", 1, false, "Let's upgrade!")
+        token := c.Publish("home/upgrade", 1, false, string(dat))
+        fmt.Println("PUBLISH TOKEN: ", reflect.TypeOf(token))
+        pubToken := token.(*mqtt.PublishToken)
+        fmt.Println("PUBLISH MSGID: ", pubToken.MessageID())
         token.Wait()
         time.Sleep(5 * time.Second)
     }
