4f97bc6fd03e700a7eb4e5cf1f8f6dd843a5fe32
Matt Everett <matt.g.everett@gmail.com>
2019-11-13 23:51:32 +0000

    Move directory read logic into OtaIndex, add GetLatest call, complete the publish logic.

--- main.go
+++ main.go
@@ -6,13 +6,10 @@ import (
     "io/ioutil"
     "log"
     "os"
-    "sort"
     "time"
     "reflect"
-    "regexp"
 
     "github.com/eclipse/paho.mqtt.golang"
-    "github.com/Masterminds/semver"
     "github.com/matt-g-everett/iotd/otaindex"
 )
 
@@ -36,11 +33,23 @@ var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
     fmt.Printf("%#v\n", versionMsg)
 }
 
+func publish(c mqtt.Client, otaIndex *otaindex.OtaIndex) {
+    v, found := otaIndex.GetLatest("logger")
+    if found {
+        dat, err := ioutil.ReadFile(fmt.Sprintf("data/ota/%s_%s.bin", "logger", v.String()))
+        check(err)
+
+        token := c.Publish("home/upgrade", 1, false, string(dat))
+        fmt.Println("PUBLISH TOKEN: ", reflect.TypeOf(token))
+        pubToken := token.(*mqtt.PublishToken)
+        fmt.Println("PUBLISH MSGID: ", pubToken.MessageID())
+        token.Wait()
+    }
+}
+
 func main() {
     mqtt.DEBUG = log.New(os.Stdout, "", 0)
     mqtt.ERROR = log.New(os.Stdout, "", 0)
-
-    binPattern := regexp.MustCompile(`^([^_]+)_(.*)\.bin$`)
 
     opts := mqtt.NewClientOptions().
         AddBroker("tcp://192.168.1.210:31883").
@@ -61,44 +70,16 @@ func main() {
         os.Exit(1)
     }
 
-    // The binary files are the ota directory
-    fileInfo, err := ioutil.ReadDir("data/ota")
-    check(err)
-
-    // Sort the binary files by software type
     otaIndex := otaindex.NewOtaIndex("data/ota")
-    for _, file := range fileInfo {
-        m := binPattern.FindStringSubmatch(file.Name())
-        version, err := semver.NewVersion(m[2])
-        check(err)
-        otaIndex.Entries[m[1]] = append(otaIndex.Entries[m[1]], version)
-    }
-
-    // Sort the binary files by version so latest is the first one
-    for softwareType, versions := range otaIndex.Entries {
-        sort.Slice(versions, func(a, b int) bool {
-            return versions[a].GreaterThan(versions[b])
-        })
-        otaIndex.Entries[softwareType] = versions
-    }
-
-    fmt.Printf("%v\n", otaIndex.Entries)
-
-    for softwareType := range otaIndex.Entries {
-        println(softwareType, ":", otaIndex.Entries[softwareType][0].String())
-    }
-
-    dat, err := ioutil.ReadFile(fmt.Sprintf("data/ota/%s_%s.bin", "logger", otaIndex.Entries["logger"][0].String()))
-    check(err)
-
     go otaIndex.WatchDirectory()
 
+    publishTimer := time.NewTicker(5 * time.Second)
     for {
-        token := c.Publish("home/upgrade", 1, false, string(dat))
-        fmt.Println("PUBLISH TOKEN: ", reflect.TypeOf(token))
-        pubToken := token.(*mqtt.PublishToken)
-        fmt.Println("PUBLISH MSGID: ", pubToken.MessageID())
-        token.Wait()
-        time.Sleep(5 * time.Second)
+        select {
+        case <-otaIndex.C:
+            log.Println("###### RELOADED")
+        case <-publishTimer.C:
+            publish(c, otaIndex)
+        }
     }
 }
--- otaindex/otaindex.go
+++ otaindex/otaindex.go
@@ -1,48 +1,89 @@
 package otaindex
 
 import (
+	"io/ioutil"
 	"log"
+	"regexp"
+	"sort"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
     "github.com/Masterminds/semver"
 )
 
+
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
 // OtaIndex contains OTA software by type and by version.
 type OtaIndex struct {
 	Entries map[string][]*semver.Version
+	C chan bool
 	watcher *fsnotify.Watcher
+	directory string
 }
 
 // NewOtaIndex creates a new OtaIndex object.
 func NewOtaIndex(directory string) *OtaIndex {
 	o := new(OtaIndex)
+	o.directory = directory
 	o.Entries = make(map[string][]*semver.Version)
+	o.C = make(chan bool)
+
 	var err error
 	o.watcher, err = fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
 	}
-	o.watcher.Add(directory)
+	o.watcher.Add(o.directory)
 	return o
+}
+
+func (o *OtaIndex) reload() {
+	binPattern := regexp.MustCompile(`^([^_]+)_(.*)\.bin$`)
+
+	// The binary files are in the ota directory
+    fileInfo, err := ioutil.ReadDir(o.directory)
+	check(err)
+
+	// Reset the Entries
+	o.Entries = make(map[string][]*semver.Version)
+
+    // Sort the binary files by software type
+    for _, file := range fileInfo {
+        m := binPattern.FindStringSubmatch(file.Name())
+        version, err := semver.NewVersion(m[2])
+        check(err)
+        o.Entries[m[1]] = append(o.Entries[m[1]], version)
+    }
+
+    // Sort the binary files by version so latest is the first one
+    for softwareType, versions := range o.Entries {
+        sort.Slice(versions, func(a, b int) bool {
+            return versions[a].GreaterThan(versions[b])
+        })
+        o.Entries[softwareType] = versions
+	}
+
+	log.Printf("%v\n", o.Entries)
+	for softwareType := range o.Entries {
+        log.Println(softwareType, ":", o.Entries[softwareType][0].String())
+    }
 }
 
 // WatchDirectory watches the directory associated with the OtaIndex.
 func (o *OtaIndex) WatchDirectory() {
-
 	debounced := debounce(o.watcher.Events)
 
 	for {
 		select {
 		case <-debounced: // event, ok := <-o.watcher.Events:
-			// if !ok {
-			// 	return
-			// }
-			// log.Println("event:", event)
-			// if event.Op & fsnotify.Write == fsnotify.Write {
-			// 	log.Println("modified file:", event.Name)
-			// }
-			log.Println("File changed")
+			o.reload()
+			o.C <- true
 		case err, ok := <-o.watcher.Errors:
 			if !ok {
 				return
@@ -55,32 +96,42 @@ func (o *OtaIndex) WatchDirectory() {
 func debounce(toDebounce chan fsnotify.Event) chan bool {
 	c := make(chan bool)
 	t := time.NewTimer(0)
-	send := false
 
 	go (func() {
 		for {
 			select {
 			case <-toDebounce:
-				log.Println("Received message")
+				// Stop the timer and drain the queue if necessary
 				t.Stop()
 				select {
 				case <-t.C:
 				default:
 				}
 
-				send = true
+				// Reset the timer
 				t.Reset(500 * time.Millisecond)
 
 			case <-t.C:
-				log.Println("Timer received")
-				if (send) {
-					log.Println("Relaying message")
-					send = false
-					c <- true
-				}
+				// The timer reached completion, relay the message
+				c <- true
 			}
 		}
 	})()
 
 	return c
+}
+
+// GetLatest gets the version for the specified software type
+func (o *OtaIndex) GetLatest(softwareType string) (version *semver.Version, found bool) {
+	var versions []*semver.Version
+	versions, found = o.Entries[softwareType]
+	if found {
+		if len(versions) > 0 {
+			version = versions[0]
+		} else {
+			found = false
+		}
+	}
+
+	return
 }
