1906543d59432bd1e32a4ce914a37a1b499babdd
Matt Everett <matt.g.everett@gmail.com>
2019-11-14 23:43:47 +0000

    Add version upgrade detection.

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+    "log"
+    "os"
+    "time"
+
+    "github.com/eclipse/paho.mqtt.golang"
     "github.com/matt-g-everett/iotd/ota"
 )
 
 func main() {
-    upgrader := ota.NewUpgrader("data/ota")
+    mqtt.DEBUG = log.New(os.Stdout, "", 0)
+    mqtt.ERROR = log.New(os.Stdout, "", 0)
+
+    options := mqtt.NewClientOptions().
+        AddBroker("tcp://***REMOVED***:31883").
+        SetClientID("iotd").
+        SetUsername("***REMOVED***").
+        SetPassword("***REMOVED***").
+        SetKeepAlive(30 * time.Second).
+        SetPingTimeout(5 * time.Second)
+
+    upgrader := ota.NewUpgrader("data/ota", options)
     upgrader.Run()
 }
--- ota/otaindex.go
+++ ota/otaindex.go
@@ -1,13 +1,13 @@
 package ota
 
 import (
-	"io/ioutil"
-	"log"
-	"regexp"
-	"sort"
-	"time"
+    "io/ioutil"
+    "log"
+    "regexp"
+    "sort"
+    "time"
 
-	"github.com/fsnotify/fsnotify"
+    "github.com/fsnotify/fsnotify"
     "github.com/Masterminds/semver"
 )
 
@@ -19,37 +19,37 @@ func check(e error) {
 
 // Index contains OTA software by type and by version.
 type Index struct {
-	Entries map[string][]*semver.Version
-	C chan bool
-	watcher *fsnotify.Watcher
-	directory string
+    Entries map[string][]*semver.Version
+    C chan bool
+    watcher *fsnotify.Watcher
+    directory string
 }
 
 // NewIndex creates a new Index object.
 func NewIndex(directory string) *Index {
-	i := new(Index)
-	i.directory = directory
-	i.Entries = make(map[string][]*semver.Version)
-	i.C = make(chan bool)
+    i := new(Index)
+    i.directory = directory
+    i.Entries = make(map[string][]*semver.Version)
+    i.C = make(chan bool)
 
-	var err error
-	i.watcher, err = fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	i.watcher.Add(i.directory)
-	return i
+    var err error
+    i.watcher, err = fsnotify.NewWatcher()
+    if err != nil {
+        log.Fatal(err)
+    }
+    i.watcher.Add(i.directory)
+    return i
 }
 
 func (i *Index) reload() {
-	binPattern := regexp.MustCompile(`^([^_]+)_(.*)\.bin$`)
+    binPattern := regexp.MustCompile(`^([^_]+)_(.*)\.bin$`)
 
-	// The binary files are in the ota directory
+    // The binary files are in the ota directory
     fileInfo, err := ioutil.ReadDir(i.directory)
-	check(err)
+    check(err)
 
-	// Reset the Entries
-	i.Entries = make(map[string][]*semver.Version)
+    // Reset the Entries
+    i.Entries = make(map[string][]*semver.Version)
 
     // Sort the binary files by software type
     for _, file := range fileInfo {
@@ -65,71 +65,71 @@ func (i *Index) reload() {
             return versions[a].GreaterThan(versions[b])
         })
         i.Entries[softwareType] = versions
-	}
+    }
 
-	log.Printf("%v\n", i.Entries)
-	for softwareType := range i.Entries {
+    log.Printf("%v\n", i.Entries)
+    for softwareType := range i.Entries {
         log.Println(softwareType, ":", i.Entries[softwareType][0].String())
     }
 }
 
 // WatchDirectory watches the directory associated with the Index.
 func (i *Index) WatchDirectory() {
-	debounced := debounce(i.watcher.Events)
+    debounced := debounce(i.watcher.Events)
 
-	for {
-		select {
-		case <-debounced:
-			i.reload()
-			i.C <- true
-		case err, ok := <-i.watcher.Errors:
-			if !ok {
-				return
-			}
-			log.Println("error:", err)
-		}
-	}
+    for {
+        select {
+        case <-debounced:
+            i.reload()
+            i.C <- true
+        case err, ok := <-i.watcher.Errors:
+            if !ok {
+                return
+            }
+            log.Println("error:", err)
+        }
+    }
 }
 
 func debounce(toDebounce chan fsnotify.Event) chan bool {
-	c := make(chan bool)
-	t := time.NewTimer(0)
+    c := make(chan bool)
+    t := time.NewTimer(0)
 
-	go (func() {
-		for {
-			select {
-			case <-toDebounce:
-				// Stop the timer and drain the queue if necessary
-				t.Stop()
-				select {
-				case <-t.C:
-				default:
-				}
+    go (func() {
+        for {
+            select {
+            case <-toDebounce:
+                // Stop the timer and drain the queue if necessary
+                t.Stop()
+                select {
+                case <-t.C:
+                default:
+                }
 
-				// Reset the timer
-				t.Reset(500 * time.Millisecond)
+                // Reset the timer
+                t.Reset(500 * time.Millisecond)
 
-			case <-t.C:
-				// The timer reached completion, relay the message
-				c <- true
-			}
-		}
-	})()
+            case <-t.C:
+                // The timer reached completion, relay the message
+                c <- true
+            }
+        }
+    })()
 
-	return c
+    return c
 }
 
 // GetLatest gets the version for the specified software type
 func (i *Index) GetLatest(softwareType string) (version *semver.Version, found bool) {
-	var versions []*semver.Version
-	versions, found = i.Entries[softwareType]
-	if found {
-		if len(versions) > 0 {
-			version = versions[0]
-		} else {
-			found = false
-		}
-	}
+    var versions []*semver.Version
+    versions, found = i.Entries[softwareType]
+    if found {
+        if len(versions) > 0 {
+            version = versions[0]
+        } else {
+            found = false
+        }
+    }
 
-	return
+    return
 }
